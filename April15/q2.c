98dcb6ab7c6d389c6b4f437deb2220f72e3094b8
Manish-rin <ms99003456789@gmail.com>
2025-04-15 17:11:41 +0530

    Solve the new bug

--- April15/q2.c
+++ April15/q2.c
@@ -1,12 +1,30 @@
-//Display students marks and gender using structure
+// Display students marks and gender using structure
 
 #include <stdio.h>
+int opt;
 struct students{
-   float marks;
-   char gender;
+    float marks;
+    char gender[30];
 };
 
-struct students s[5];
+struct students s[2];
 int main(){
-    
+    for (int i = 0; i < 2; i++){
+        printf("Enter the marks of student%d: ", i + 1);
+        scanf("%f",&s[i].marks);
+        
+        printf("Enter the gender of student%d: ", i + 1);
+        scanf(" %c",&s[i].gender);
+
+        printf("\n");
+    }
+
+    //Ask user to choose whose details he want to see
+    printf("Whose details you want to see? (eg Student1, student2,etc):\n");
+    printf("Enter the serial number of the student to display his/her details: ");
+    scanf("%d",&opt);
+
+    //Display details
+    printf("\n--------DETAILS---------\n");
+    printf("Marks: %f\nGender: %s",s[opt].marks,s[opt].gender);
 }
