a7441bb4499119139e3cf4982d466fd38ccaebf5
Manish Singh <ms99003456789@gmail.com>
2025-04-27 13:25:34 +0530

    All Perfect

--- 27March (NEW)/menu.c
+++ 27March (NEW)/menu.c
@@ -7,36 +7,31 @@ c. display the array.*/
 #include <stdio.h>
 //To determine the size of array
 int n(int *x){
-    int *ptr = x;
     int count = 0;
-    for(int i = 0; i<=20;i++){
-        if(ptr[i]==0){
-            break;
+    for(int i = 0; i<20;i++){
+        if(x[i]!=0){
+            count = count + 1;
         }
-        count = count + 1;
-        ptr++;
     }
     return count; //This is the size of array
-
 }
 
-int insert_ele(int position, int element,int *count, int x
+int insert_ele(int position, int element,int count, int *x
 ){
-    for(int i = 1; i>position; i++){
+    for(int i = 1; i + 1 <position; i++){
         //shifting position of elements
-        x[count+2-i] = x[count+i]
+        x[count-i] = x[count - i +1];
     }
 
-
-
+    x[position] = element;
+    printf("The new element at index %d: %d ",position,x[position]);
 }
 
 int main(){
-    int n,position = 2,element = 87;
-    int x[20] = {5,8,9,2};
+    int position = 2,element = 87;
+    int x[20] = {78,8,9,2};
     // n = sizeof(x)/sizeof(x[0]);
-    insert_ele(position,element,*x,x[20]);
-
+    insert_ele(position,element,n(x),x);
 }
 
 
